49dd609ed1412989e2acfe03cc77edbc82aa1d6f
Rudinei de Brito <rudineidebrito18@gmail.com>
2023-04-29 10:58:29 -0300

    💄 add scroll control in hamburger button

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ darkMode.addEventListener('change', () => {
     checked = !checked;
     console.log(darkMode.checked);
 });
+
+const menuLinks = document.querySelectorAll('a.menuLink');
+const menuChecked = document.getElementById('hamburgerButtonCheck')
+
+function bodyScrollBar(state) {
+    const body = document.getElementsByTagName('body')[0]
+    body.style.overflow = state
+}
+
+menuChecked.addEventListener('change', () => {
+    if (menuChecked.checked) {
+        bodyScrollBar('hidden')
+    } else {
+        bodyScrollBar('auto')
+    }
+})
+
+menuLinks.forEach(e => {
+    e.addEventListener('click', event => {
+        event.preventDefault()
+        console.log({menuChecked})
+        if (menuChecked.checked) {
+            menuChecked.checked = false
+        }
+        bodyScrollBar('auto')
+    })
+});
