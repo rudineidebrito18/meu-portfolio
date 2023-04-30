25bdb0151fbc8b09db802582e6e9f815b0d79a26
Rudinei de Brito <rudineidebrito18@gmail.com>
2023-04-30 12:45:57 -0300

    ✨ add news techs, update scrooll

--- script.js
+++ script.js
@@ -11,33 +11,22 @@ const menuLinks = document.querySelectorAll('a.menuLink');
 const menuChecked = document.getElementById('hamburgerButtonCheck')
 const body = document.getElementsByTagName('body')[0]
 
-// function bodyScrollBar(state) {
-//     body.style.overflowY = state
-// }
-
-// menuChecked.addEventListener('change', () => {
-//     if (menuChecked.checked) {
-//         bodyScrollBar('hidden')
-//     } else {
-//         bodyScrollBar('auto')
-//     }
-// })
-
 menuLinks.forEach(e => {
     e.addEventListener('click', event => {
         event.preventDefault()
         const tamScreen = root.offsetHeight
         if (menuChecked.checked) {
             menuChecked.checked = false
-        } 
-        scrollToIdOnClick(event)
-        // bodyScrollBar('auto')
+            scrollToIdOnClick(event, tamScreen / 3)
+        } else {
+            scrollToIdOnClick(event, 80)
+        }
     })
 });
 
-function scrollToIdOnClick(event) {
+function scrollToIdOnClick(event, amount = 0) {
     const id = event.target.getAttribute('href')
     const to = document.querySelector(id).offsetTop
     
-    body.scroll(0, to - 80)
+    body.scroll(0, to - amount)
 }
